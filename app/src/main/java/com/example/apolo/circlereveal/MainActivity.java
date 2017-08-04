dd084a31c4dd84dd711a55d2a144fc6ef529c861
Apolo <apolorossi@gmail.com>
2017-08-04 12:32:53 -0300

    workin

--- app/src/main/java/com/example/apolo/circlereveal/MainActivity.java
+++ app/src/main/java/com/example/apolo/circlereveal/MainActivity.java
@@ -5,25 +5,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewAnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private ImageView revealImage;
+    private Button btnReveal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         revealImage = (ImageView) findViewById(R.id.revealImage);
-        revealImage.post(new Runnable() {
+        btnReveal = (Button) findViewById(R.id.btnReveal);
+
+        btnReveal.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
+            public void onClick(View view) {
                 showAnimation();
             }
         });
+        /*revealImage.post(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }); */
     }
 
     private void showAnimation() {
@@ -33,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         int finalRadius = Math.max(revealImage.getWidth(),revealImage.getHeight());
 
         Animator anim = ViewAnimationUtils.createCircularReveal(revealImage,cx,cy,0,finalRadius);
-        anim.setDuration(5000);
+        anim.setDuration(1000);
         revealImage.setVisibility(View.VISIBLE);
         anim.start();
     }
